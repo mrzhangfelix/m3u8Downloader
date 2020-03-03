9244081c8610e29c42f4270b0fd67358dadabf35
felix <805908873@qq.com>
2020-03-03 17:41:35 +0800

    Create util.py

---
+++ client/util.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+
+import urllib.parse
+import sys
+
+from wxpython import sessionutil
+
+
+# 展示进度条
+def show_progress(percent):
+    bar_length = 50
+    hashes = '#' * int(percent / 100 * bar_length)
+    spaces = ' ' * (bar_length - len(hashes))
+    sys.stdout.write("\rPercent: [%s] %.2f%%" % (hashes + spaces, percent))
+    sys.stdout.flush()
+
+
+# 用于获取最新队列
+def get_real_url(m3u8_url):
+    r = sessionutil.session.get(m3u8_url, timeout=10)
+    if r.ok:
+        body = r.content.decode()
+        if body:
+            ts_url = ''
+            body_list = body.split('\n')
+            for n in body_list:
+                if n and not n.startswith("#"):
+                    ts_url = urllib.parse.urljoin(m3u8_url, n.strip())
+            if ts_url != '':
+                print('真实地址为' + ts_url)
+                return ts_url
+    else:
+        print(r.status_code)
