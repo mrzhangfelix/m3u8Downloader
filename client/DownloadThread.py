e546e651b5436aa38bdb2d46b5bf72b1ee731c9e
felix <805908873@qq.com>
2020-03-04 17:37:57 +0800

    Update DownloadThread.py

--- client/DownloadThread.py
+++ client/DownloadThread.py
@@ -4,10 +4,11 @@ import threading
 
 from PyQt5.QtCore import QThread, pyqtSignal
 
-from client import constant, sessionUtil, util
+from client import constant, sessionutil, util
 
 
 class DownloadThread(QThread):
+    # 传递当前进度的百分比
     signalpercent = pyqtSignal(int)
 
     def __init__(self, threadID, name, q):
@@ -23,7 +24,7 @@ class DownloadThread(QThread):
     def run(self):
         self.download_data(self.q)
 
-    # 下载数据
+    # 下载数据，写入临时文件
     def download_data(self, q):
         while not constant._exitFlag:
             constant._queueLock.acquire()
@@ -34,16 +35,18 @@ class DownloadThread(QThread):
                 retry = 3
                 while retry:
                     try:
-                        r = sessionUtil.session.get(url, timeout=20)
+                        r = sessionutil.session.get(url, timeout=20)
                         if r.ok:
                             file_name = url.split('/')[-1].split('?')[0]
-                            # print(file_name)
                             with open(os.path.join(constant._dir, file_name), 'wb') as f:
                                 f.write(r.content)
+                            # 访问公共资源_count,需要获取线程锁
                             constant._queueLock.acquire()
                             constant._count = constant._count + 1
                             percent = int(constant._count / constant._ts_total * 100)
-                            self.signalpercent.emit(percent)
+                            if percent != constant._percent:
+                                constant._percent = percent
+                                self.signalpercent.emit(percent)
                             util.show_progress(percent)
                             constant._queueLock.release()
                             break
